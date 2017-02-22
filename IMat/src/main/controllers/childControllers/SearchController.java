db65a4c6115f712c98a687c80c59e6c418e271b7
Pontus Stjernström <pontus@stjernstrom.nu>
2017-02-22 17:27:51 +0100

    Fixed bug with login panel not being clickable. Also added skeleton for search bar.

---
+++ IMat/src/main/controllers/childControllers/SearchController.java
@@ -0,0 +1,21 @@
+package main.controllers.childControllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Pontus on 2017-02-22.
+ */
+public class SearchController implements Initializable {
+    @FXML TextField txtSearch;
+    @FXML Button btnSearch;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
