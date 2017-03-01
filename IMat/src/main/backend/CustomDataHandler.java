bccafccc2e7c4e96f6f880e4a89fe34a4afddd76
Pontus Stjernström <pontus@stjernstrom.nu>
2017-03-01 19:03:31 +0100

    Smallest commit so far. Mentally started extension of User and Customer where they should be connected.

--- IMat/src/main/backend/CustomDataHandler.java
+++ IMat/src/main/backend/CustomDataHandler.java
@@ -25,79 +25,85 @@ public class CustomDataHandler extends BackendWrapper {
 
     private ObservableList<Product> displayedProducts;
 
-    public static CustomDataHandler getInstance(){
-        if(instance == null)
+    public static CustomDataHandler getInstance() {
+        if (instance == null)
             instance = new CustomDataHandler();
 
         return instance;
     }
 
-    private CustomDataHandler(){
+    private CustomDataHandler() {
         super();
         displayedProducts = FXCollections.observableArrayList();
 
-        if(!getUserHandler().isLoggedIn()){
+        if (!getUserHandler().isLoggedIn()) {
             getShoppingCart().getItems().clear();
         }
     }
 
-    public UserHandler getUserHandler(){
-        if(userHandler == null)
+    public UserHandler getUserHandler() {
+        if (userHandler == null)
             userHandler = new UserHandler(getUser(), getCustomer(), imatDirectory());
 
         return userHandler;
     }
 
-    public ObservableList<Product> getDisplayedProducts(){
+    public ObservableList<Product> getDisplayedProducts() {
         return displayedProducts;
     }
+
     @Override
-    public List<Product> findProducts(String s){
-    	List<Product> products = super.getProducts();
-    	List<Product> result = new ArrayList<>();
-    	for(Product p: products){
-    		if(p.getName().equalsIgnoreCase(s)){
-    			result.add(p);
-    		}
-    		
-    	}
-    	for(Product p: products){
-    		if(p.getName().startsWith(s)){
-    			if(!result.contains(p)){
-    				result.add(p);
-    			}
-    			
-    		}
-    		
-    	}
-    	for(Product p: products){
-    		String[] words = p.getName().split(" ");
-    		for(String word: words){
-    			if(word.equalsIgnoreCase(s)){
-    				if(!result.contains(p)){
-        				result.add(p);
-        			}
-    			}
-    			
-    		
-    		if(word.toLowerCase().startsWith(s.toLowerCase())){
-    			System.out.println(p.getName());
-    			if(!result.contains(p)){
-    				result.add(p);
-    			}
-    			
-    		}
-    		if(word.toLowerCase().endsWith(s.toLowerCase())){
-    			if(!result.contains(p)){
-    				result.add(p);
-    			}
-    		}
-    		}
-    	}
-    	
-    	
-    	return result;
-    	//return super.findProducts(s);
+    public List<Product> findProducts(String s) {
+        List<Product> products = super.getProducts();
+        List<Product> result = new ArrayList<>();
+        for (Product p : products) {
+            if (p.getName().equalsIgnoreCase(s)) {
+                result.add(p);
+            }
+
+        }
+        for (Product p : products) {
+            if (p.getName().startsWith(s)) {
+                if (!result.contains(p)) {
+                    result.add(p);
+                }
+
+            }
+
+        }
+        for (Product p : products) {
+            String[] words = p.getName().split("\\s+");
+            for (String word : words) {
+                if (word.equalsIgnoreCase(s)) {
+                    if (!result.contains(p)) {
+                        result.add(p);
+                    }
+                }
+
+
+                if (word.toLowerCase().startsWith(s.toLowerCase())) {
+                    System.out.println(p.getName());
+                    if (!result.contains(p)) {
+                        result.add(p);
+                    }
+
+                }
+                if (word.toLowerCase().endsWith(s.toLowerCase())) {
+                    if (!result.contains(p)) {
+                        result.add(p);
+                    }
+                }
+            }
+        }
+
+
+        return result;
+        //return super.findProducts(s);
+    }
+
+    @Override
+    public List<Order> getOrders(){
+        return super.getOrders();
     }
 
 }
---
+++ IMat/src/main/backend/UserOrder.java
@@ -0,0 +1,11 @@
+package main.backend;
+
+import se.chalmers.ait.dat215.project.Order;
+import se.chalmers.ait.dat215.project.User;
+
+/**
+ * Created by pontu on 2017-03-01.
+ */
+public class UserOrder extends Order {
+    private User user;
+}
