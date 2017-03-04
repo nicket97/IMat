03b9c5df35f6ec11a47f26d2b4450d73c62a28cb
Felix <felixnorden@gmail.com>
2017-03-04 22:49:18 +0100

    Added scroll-filtering maneuverability with listPane in ProductView

--- IMat/src/main/controllers/childControllers/ProductViewController.java
+++ IMat/src/main/controllers/childControllers/ProductViewController.java
@@ -15,6 +15,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import se.chalmers.ait.dat215.project.IMatDataHandler;
@@ -29,13 +30,11 @@ public class ProductViewController implements Controllable{
     @FXML
     private AnchorPane productView;
     @FXML
-    private Label subcatLabel;
-    @FXML
-    private FlowPane productViewNodePane;
-    @FXML
     private Label labelHeader;
     @FXML
     private VBox listBox;
+    @FXML
+    private ScrollPane scrollPane;
     
     public void displayProducts (List<Product> productList, String label) {
         setVisible(true);
@@ -49,14 +48,11 @@ public class ProductViewController implements Controllable{
             	if(!usedCat.contains(productList.get(i).getCategory())){
             		ProductViewSubCategory searchResult = new ProductViewSubCategory(productList, label);
             		productViewFlowPane.getChildren().add(searchResult);
-            		labelHeader.setText(label);
+            		
+                        labelHeader.setText(label);
             		usedCat.add(productList.get(i).getCategory());
             		System.out.println(usedCat.toString());
             	}
-                
-                
-                
-                
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -70,17 +66,38 @@ public class ProductViewController implements Controllable{
         if (productViewFlowPane != null) {
             productViewFlowPane.getChildren().clear();
             listBox.getChildren().clear();
+            scrollPane.setVvalue(0);
         }
+        
+        // Array with +1 length for setting 0 as starting point and also getting full height of nodes
+        double[] scrollIndex = new double[section.length + 1];
         for(int i = 0;i<section.length;i++) {
             try {
                 ProductViewSubCategory subSection = new ProductViewSubCategory(section[i]);
                 productViewFlowPane.getChildren().add(subSection);
+                
+                // Add each height to their respective index, starting from 1 to max, adding each previous height.
+                scrollIndex[i+1] = scrollIndex[i] + productViewFlowPane.getChildren().get(i).getBoundsInLocal().getHeight();
                 labelHeader.setText(label);
                 listBox.getChildren().add(createSubcategoryButton(subSection.getSubCatLabel()));
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
+        }
+        
+        
+        double scrollPaneLength = scrollIndex[scrollIndex.length-1];
+        // Generate theoretical offset, based on the amount of elements and the loss of element height.
+        double offset = scrollIndex[1]/(scrollIndex.length - 3) ;
+       
+        // Loop through each list-category, adding a listener with their index + offset divided by the whole window.
+        // The interval needed is 0 - 1, problem is that 1 pane is "lost" as Vvalue = 1 is bottom of scrollbar at bottom
+        // and Vvalue = 0 is the top of the scrollbar at the top.
+        for(int index = 0; index < listBox.getChildren().size(); index++){
+            int i = index;
+            listBox.getChildren().get(i).setOnMouseClicked(e -> scrollPane.setVvalue((scrollIndex[i] + offset * (i))/ scrollPaneLength));
         }
         
     }
